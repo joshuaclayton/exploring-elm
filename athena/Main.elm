d8fe067bebeabc953233a366768d8866bfd8585e
Joshua Clayton <jclayton@thoughtbot.com>
2016-02-26 17:15:18 -0500

    do something

--- athena/Main.elm
+++ athena/Main.elm
@@ -14,10 +14,18 @@ type Action
 
 type alias Model = { topics: List Topic  }
 
+type alias Item =
+  { id: String
+  , name: String
+  , description: String
+  , requesting_feedback: Bool
+  }
+
 type alias TeachingMove =
   { id: String
   , location_in_topic: String
   , line_number: String
+  , items: List Item
   }
 
 type alias Topic =
@@ -39,7 +47,14 @@ initialModel = { topics = [] }
 nullTopic = { id = "", locations = [], name = "", teaching_moves = [] }
 
 nullTeachingMove : TeachingMove
-nullTeachingMove = { id = "", location_in_topic = "", line_number = "" }
+nullTeachingMove = { id = "", location_in_topic = "", line_number = "", items = [] }
+
+item : String -> Decoder Item
+item id =
+  succeed (Item id)
+    |: ("name" := oneOf [ string, null "" ])
+    |: ("description" := oneOf [ string, null "" ])
+    |: ("requesting-feedback" := oneOf [ bool, null False ])
 
 topic : String -> Decoder Topic
 topic id =
@@ -53,35 +68,76 @@ teachingMove id =
   succeed (TeachingMove id)
     |: ("location-in-topic" := oneOf [ string, null ""])
     |: ("line-number" := oneOf [ string, null ""])
+    |: (succeed [])
 
 noEffects : a -> (a, Effects b)
 noEffects thing = (thing, Effects.none)
 
 init = (initialModel, getTopics)
 
-processTeachingMove : JsonApiPayload -> TeachingMove
-processTeachingMove teachingMovePayload =
-  case decodeValue (teachingMove teachingMovePayload.id) teachingMovePayload.attributes of
-    Ok val -> val
+
+
+
+
+
+
+
+
+
+
+processDomainEntity : (String -> Decoder a) -> (a -> List JsonApiPayload -> JsonApiPayload -> a) -> List JsonApiPayload -> JsonApiPayload -> a
+processDomainEntity genericDecoder relationshipProcessor includedRecords payload =
+  case decodeValue (genericDecoder payload.id) payload.attributes of
+    Ok decodedValue' -> relationshipProcessor decodedValue' includedRecords payload
     Err message -> crash message
 
-processTopic : JsonApiBody -> JsonApiPayload -> Topic
-processTopic body topicPayload =
-  case decodeValue (topic topicPayload.id) topicPayload.attributes of
-    Ok topic' -> processTopicRelationships topic' body.included topicPayload
-    Err message -> crash message
 
-handle : String -> List JsonApiPayload -> JsonApiPayload -> List TeachingMove
-handle filter includedRecords primaryRecord =
-  let relationshipIds = relationshipIdsByType filter primaryRecord.relationships
-      filterRecordsToRelationship = (\record -> List.member record.id relationshipIds)
-      filteredRecords = List.filter filterRecordsToRelationship (filterListByType filter includedRecords)
-  in List.map processTeachingMove filteredRecords
+
+processTeachingMoveRelationships : TeachingMove -> List JsonApiPayload -> JsonApiPayload -> TeachingMove
+processTeachingMoveRelationships teachingMove included payload =
+  let items = log "items" (handleItems "items" included payload)
+  in { teachingMove | items = items }
 
 processTopicRelationships : Topic -> List JsonApiPayload -> JsonApiPayload -> Topic
 processTopicRelationships topic included payload =
   let teachingMoves = log "moves" (handle "teaching-moves" included payload)
+      includedLength = log "included length" (included |> List.length)
   in { topic | teaching_moves = teachingMoves }
+
+nullRelationshipProcessor : a -> List JsonApiPayload -> JsonApiPayload -> a
+nullRelationshipProcessor generic included genericPayload =
+  generic
+
+handle : String -> List JsonApiPayload -> JsonApiPayload -> List TeachingMove
+handle filter includedRecords primaryRecord =
+  let relationshipIds = relationshipIdsByType filter primaryRecord.relationships
+      includedLength = log "included length in 'handle'" (includedRecords |> List.length)
+      filterRecordsToRelationship = (\record -> List.member record.id relationshipIds)
+      filteredRecords = List.filter filterRecordsToRelationship (filterListByType filter includedRecords)
+  in List.map (processDomainEntity teachingMove processTeachingMoveRelationships includedRecords) filteredRecords
+
+
+handleItems : String -> List JsonApiPayload -> JsonApiPayload -> List Item
+handleItems filter includedRecords primaryRecord =
+  let relationshipIds = log ("relationshipIds for " ++ filter) (relationshipIdsByType filter primaryRecord.relationships)
+      filterRecordsToRelationship = (\record -> List.member record.id relationshipIds)
+      filteredRecords = List.filter filterRecordsToRelationship (filterListByType filter includedRecords)
+      recordsLength = log "records length" (includedRecords |> List.head)
+  in List.map (processDomainEntity item nullRelationshipProcessor includedRecords) filteredRecords
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
@@ -89,7 +145,7 @@ update action model =
     NoOp -> noEffects model
     NewResponse data ->
       let payload = Maybe.withDefault nullJsonApiBody data
-      in noEffects { model | topics = (filterPayloadByType payload.data "topics" (processTopic payload)) }
+      in noEffects { model | topics = (filterPayloadByType payload.data "topics" (processDomainEntity topic processTopicRelationships payload.included)) }
 
 inputs : List (Signal Action)
 inputs = []
@@ -113,8 +169,15 @@ renderTopic topic =
     []
     [ h2 [] [text (topic.name ++ " (id: " ++ topic.id ++ ")")]
     , ul [] (List.map (\location -> li [] [text location]) topic.locations)
-    , ul [] (List.map (\teaching_move -> li [] [text (teaching_move.line_number ++ " - " ++ teaching_move.location_in_topic)]) topic.teaching_moves)
+    , ul [] (List.map renderTeachingMove topic.teaching_moves)
     ]
+
+renderTeachingMove : TeachingMove -> Html
+renderTeachingMove teachingMove =
+  li [] [
+    text (teachingMove.line_number ++ " - " ++ teachingMove.location_in_topic)
+  , div [] (List.map (\item -> text item.name) teachingMove.items)
+  ]
 
 app = StartApp.start { init = init, view = view, update = update, inputs = inputs }
 
